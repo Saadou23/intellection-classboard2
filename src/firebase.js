a28ddac0a72e086528569631d63d3e10cdb8d3d3
saadou23 <saaddalili@gmail.com>
2026-01-20 21:50:49 +0100

    Update to Firebase v2 with optimized rules

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwQNS9uWUtBYMqUKb2wWFtpZLNLW97Hwg",
-  authDomain: "intellection-classboard-b2ee6.firebaseapp.com",
-  projectId: "intellection-classboard-b2ee6",
-  storageBucket: "intellection-classboard-b2ee6.firebasestorage.app",
-  messagingSenderId: "36437442866",
-  appId: "1:36437442866:web:9359f6c84d71a5dc56d72b"
+  apiKey: "AIzaSyDNUD0aXKN3zdyKxUGqMgdBbW5Poomdros",
+  authDomain: "intellection-classboard-v2.firebaseapp.com",
+  projectId: "intellection-classboard-v2",
+  storageBucket: "intellection-classboard-v2.firebasestorage.app",
+  messagingSenderId: "401219921591",
+  appId: "1:401219921591:web:3121a89c13129a5192cf32"
 };
 
 // Initialize Firebase
